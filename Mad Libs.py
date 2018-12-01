782a62599666c4da7a9626c3a37bcc291add3c4c
kstam006 <43752420+kstam006@users.noreply.github.com>
2018-11-30 20:02:32 -0500

    Add files via upload

---
+++ Mad Libs.py
@@ -0,0 +1,52 @@
+file:///C:/Users/owner/AppData/Local/WhatsApp/app-0.3.1409/resources/app.asar/index.html#adjective1 = raw_input("Enter an adjective. ")
+
+print "Signs point to a very " + adjective1 + " yes. "
+
+adjective2= raw_input("Enter an adjective. ")
+
+print "Signs point to a very " + adjective2 + " no. "
+
+person1 = raw_input("Enter a name." )
+
+print "Don't believe anything " + person1 + " says." 
+
+partofbody1 = raw_input("Enter a body part. ")
+
+print "What does your " + partofbody1 + " tell you?" 
+
+adjective2 = raw_input("Enter an adjective. ")
+singularnoun1 = raw_input("Enter a noun. ")
+
+print "Picture a/an " + adjective2 + " " + singularnoun1 + ". That is your answer. "
+
+clothing1 = raw_input("Enter an article of clothing.") 
+
+print "You will find the answer in your " + clothing1 + "." 
+
+number1 = raw_input("Enter a number greater than one.")
+noun2 = raw_input("Enter a plural noun. ")
+
+print "I see " + number1 + " big " + noun2 + " in your future. " 
+
+adjective3 = raw_input("Enter an adjective.")
+
+print "The skies are " + adjective3 + ". The future is uncertain. "
+
+yourname1 = raw_input("Enter your name. ")
+adverb1 = raw_input("Enter an adverb. ")
+
+print " Dear " + yourname1 + ", You are " + adverb1 + " invited!"
+
+
+theme1 = raw_input("Enter a theme. ")
+place1 = raw_input("Enter a place. ")
+dayofweek1 = raw_input("Enter a day of the week. ")
+time1 = raw_input("Enter a time. ")
+verb1 = raw_input("Enter a verb. ")
+animal1 = raw_input("Enter an animal. ")
+contact1 = raw_input("Enter a contact number. ")
+
+print name1 + " is having a " +theme1 + " party! "
+print " It's going to be at " + place1 + " on " + dayofweek1 + ". " 
+print " Please make sure to show up at " + time1 + ", or else you will be required to " + verb1 + " a/an " + animal1 + " with your " + partofbody1 + "." 
+print " RSVP at " + contact1 + "."
